f2110848c7e24fe2f46a8bb882488872d190141c
myflying <512710257@qq.com>
2018-06-29 18:48:25 +0800

    update 1.0.1 version

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,13 +3,22 @@ var username
 var sexType = 1
 var simg = ''
 var stitle = ''
+var rtitle = ''
+var share_id = ''
+
+var new_app_id
+var new_pre_img
+var float_img = '../images/float.gif'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    app_id: '',
+    is_nav: true,
+    floatImg: float_img,
+    isUse: true
   },
   inputname: function(e) {
     username = e.detail.value
@@ -19,33 +28,78 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    var Page$this = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        var result = Page$this.compareVersion(res.SDKVersion, '2.0.7')
+        Page$this.setData({
+          isUse: result >= 0 ? true : false
+        })
+      },
+    })
+
     wx.setNavigationBarTitle({
       title: '人生成绩单'
     })
-
+    var Page$this = this;
     wx.request({
       url: 'https://cj.198254.com/api/v1.game/getAppInfo',
       method: 'POST',
       data: {
         'app_id': 1
       },
-      success: function (res) {
-        //console.log(res.data.data.share_title)
-        var index = Math.floor(Math.random() * 2);
-        if (res.data.data && res.data.data.share_ico){
-          var simgs = res.data.data.share_ico;
-          var stitles = res.data.data.share_title;
-          if (simgs && simgs.length > 1 && stitles && stitles.length > 1){
-            simg = simgs[index]
-            stitle = stitles[index]
+      success: function(res) {
+        if (res.data.data.more_app_info){
+          float_img = res.data.data.more_app_info[0].img
+          new_app_id = res.data.data.more_app_info[0].url;
+          new_pre_img = res.data.data.more_app_info[0].xcx_img
+          if (new_app_id) {
+            Page$this.setData({
+              is_nav: true,
+              app_id: new_app_id
+            })
+          } else {
+            Page$this.setData({
+              is_nav: false
+            })
+          }
+
+          if (float_img) {
+            Page$this.setData({
+              floatImg: float_img
+            })
+          }
+
+          var index = Math.floor(Math.random() * 2);
+          if (res.data.data && res.data.data.share_ico) {
+            var simgs = res.data.data.share_ico;
+            var stitles = res.data.data.share_title;
+            share_id = res.data.data.share_id
+            if (simgs && simgs.length > 1 && stitles && stitles.length > 1) {
+              simg = simgs[index]
+              stitle = stitles[index]
+              rtitle = stitles[2]
+            }
           }
         }
       },
-      fail: function (res) {
+      fail: function(res) {
         console.log(res)
       }
     })
+  },
 
+  newApp: function(e) {
+    if (!new_app_id) {
+      wx.previewImage({
+        urls: [new_pre_img],
+        current: new_pre_img
+      })
+    }else{
+      wx.navigateToMiniProgram({
+        appId: new_app_id
+      })
+    }
   },
 
   radioChange: function(e) {
@@ -75,11 +129,12 @@ Page({
         'sex': sexType
       },
       success: function(res) {
+        console.log(res.data)
         wx.hideLoading()
         var img = res.data.data.img;
-        if(res.data.data){
+        if (res.data.data) {
           wx.navigateTo({
-            url: '../result/result?img=' + img + '&simg=' + simg + '&stitle='+ stitle
+            url: '../result/result?img=' + img + '&rtitle=' + rtitle + "&share_id=" + share_id
           })
         }
       },
@@ -99,5 +154,31 @@ Page({
       path: '/pages/home/home',
       imageUrl: simg
     }
+  },
+  compareVersion: function(v1, v2) {
+    v1 = v1.split('.')
+    v2 = v2.split('.')
+    var len = Math.max(v1.length, v2.length)
+
+    while (v1.length < len) {
+      v1.push('0')
+    }
+    while (v2.length < len) {
+      v2.push('0')
+    }
+
+    for (var i = 0; i < len; i++) {
+      var num1 = parseInt(v1[i])
+      var num2 = parseInt(v2[i])
+
+      if (num1 > num2) {
+        return 1
+      } else if (num1 < num2) {
+        return -1
+      }
+    }
+
+    return 0
   }
+
 })
--- pages/result/result.js
+++ pages/result/result.js
@@ -1,15 +1,17 @@
 // pages/result/result.js
-var simg = ''
-var stitle = ''
-var pre_img
+var rtitle = ''
+var pre_img = ''
+var share_id = ''
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    img_url: '',
-    is_show : true
+    img_url: '../images/def_bg.png',
+    is_show : true,
+    is_load_done:false,
+    is_share : true
   },
 
   /**
@@ -29,30 +31,57 @@ Page({
       }
     })
 
-    simg = options.simg
-    stitle = options.stitle
     pre_img = options.img
+    rtitle = options.rtitle
+    share_id = options.share_id
+
+    if (options.isShare && options.isShare == 1){
+      this.setData({
+        is_share : false
+      })
+    }
 
     console.log('result img --->' + pre_img)
     wx.setNavigationBarTitle({
       title: '你的人生成绩单',
     })
 
+    wx.showLoading({
+      title: '打印中···',
+    })
+
     if (pre_img) {
-      this.setData({
-        img_url: pre_img
+      // this.setData({
+      //   img_url: pre_img
+      // })
+      wx.getImageInfo({
+        src: pre_img,
+        success:function(res){
+          wx.hideLoading()
+          console.log(res)
+          Page$this.setData({
+            is_load_done:true,
+            img_url: res.path
+          })
+        },
+        fail:function(e){
+          wx.hideLoading()
+        }
       })
     }
   },
-
+  home:function(e){
+    wx.navigateTo({
+      url: '/pages/home/home',
+    })
+  },
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function() {
     return {
-      title: stitle,
-      path: '/pages/home/home',
-      imageUrl: simg
+      title: rtitle,
+      path: '/pages/result/result?img=' + pre_img + "&rtitle=" + rtitle + "&share_id=" + share_id + "&isShare=1"
     }
   },
 
@@ -76,6 +105,94 @@ Page({
       urls: [pre_img],
       current: pre_img
     })
+  },
+  saveImg:function(){
+    var Page$this = this;
+    wx.getSetting({
+      success(res) {
+        console.log(res)
+        if (!res.authSetting['scope.writePhotosAlbum']) {
+          wx.authorize({
+            scope:
+            'scope.writePhotosAlbum',
+            success() {
+              Page$this.downimage();
+            }
+          })
+        }else{
+          Page$this.downimage();
+        }
+      },
+      fail(err){
+        console.log(err)
+        if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
+          console.log("用户一开始拒绝了，我们想再次发起授权")
+          console.log('打开设置窗口')
+          wx.openSetting({
+            success(settingdata) {
+              console.log(settingdata)
+              if (settingdata.authSetting['scope.writePhotosAlbum']) {
+                console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
+                Page$this.downimage();
+              }
+              else {
+                console.log('获取权限失败，给出不给权限就无法正常使用的提示')
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  downimage: function () {
+    
+    if(pre_img){
+      wx.showLoading({
+        title: '文件下载中',
+      })
+      //文件下载
+      wx.downloadFile({
+        url: pre_img,
+        success:
+        function (res) {
+          console.log(res);
+          //图片保存到本地
+          wx.saveImageToPhotosAlbum({
+            filePath: res.tempFilePath,
+            success: function (data) {
+              wx.hideLoading()
+              console.log("save success--->" + data);
+              wx.showToast({
+                title: '图片已保存',
+              })
+            },
+            fail: function (err) {
+              wx.hideLoading()
+              console.log(err);
+              if (err.errMsg === "saveImageToPhotosAlbum:fail auth deny") {
+                console.log("用户一开始拒绝了，我们想再次发起授权")
+                console.log('打开设置窗口')
+                wx.openSetting({
+                  success(settingdata) {
+                    console.log(settingdata)
+                    if (settingdata.authSetting['scope.writePhotosAlbum']) {
+                      console.log('获取权限成功，给出再次点击图片保存到相册的提示。')
+                    }
+                    else {
+                      console.log('获取权限失败，给出不给权限就无法正常使用的提示')
+                    }
+                  }
+                })
+              }
+            }
+          })
+        }
+      })
+    }else{
+      wx.showToast({
+        title: '保存失败，请稍后再试',
+      })
+    }
   }
 
 })
